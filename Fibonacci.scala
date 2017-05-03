eae2180f3c1b0d528e746a35b7a22aa5c83e9279
xinvincible21 <xinvincible21@gmail.com>
2017-05-03 17:01:38 -0400

    Update Fibonacci.scala

--- Fibonacci.scala
+++ Fibonacci.scala
@@ -10,6 +10,25 @@ object Fibonacci{
       recursive(i - 1) + recursive(i - 2)
     }
   }
+  
+  
+def fibIter(prev:Int, current:Int, n:Int):Int = {
+    if(n == 0){
+    	current
+    }
+    else {
+    	fibIter(current, prev + current, n - 1)
+    }
+}
+
+
+def fibWithTailRec(n:Int) = {
+    if (n > 1) {
+    	fibIter(1, 1, n - 2)
+    } else {
+    	n
+    }
+}
 
   def iterative(i:Int) : Int = {
 
